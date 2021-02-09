699c552400eec5ae303ef5234b89018a585305fa
Carlos Martins <carlosemartins97@gmail.com>
2021-02-09 20:55:20 -0300

    Add navbar

---
+++ front-web/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainNav from './core/components/MainNav';
+
+function App() {
+  return (
+    <div className="App">
+      <MainNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-web/src/core/components/MainNav/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap'
+import './styles.css';
+
+const MainNav = () => {
+    return (
+        <div className="navbar-container">
+            <Navbar variant="dark" bg="primary" expand="lg" className="main-nav font-weight-bold">
+                <Navbar.Brand href="/">Listz</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav nav-block">
+                    <Nav className="mx-auto nav-items">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Consulta</Nav.Link>
+                        <Nav.Link href="#teste">Lista Completa</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default MainNav;
